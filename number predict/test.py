d0f68c52ee871ad09c7b276222b7c723eff70bdf
ess-2333 <a13726423662@gmail.com>
2026-03-03 10:30:58 +0800

    Add files via upload

---
+++ number predict/test.py
@@ -0,0 +1,88 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+
+class Net(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # 全链接层
+        self.fc1 = torch.nn.Linear(28*28, 64)
+        self.fc2 = torch.nn.Linear(64, 64)
+        self.fc3 = torch.nn.Linear(64, 64)
+        self.fc4 = torch.nn.Linear(64, 10)
+
+    def forward(self, x):
+        # 对fc线性计算
+        # 激活函数torch.nn.functional.relu（非线性）
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = torch.nn.functional.relu(self.fc3(x))
+        # softmax归一化，前log为对数运算（提高稳定性）
+        x = torch.nn.functional.log_softmax(self.fc4(x), dim=1)
+        return x
+
+# 导入数据
+def get_data_loader(is_train):
+    # tensor（张量）
+    to_tensor = transforms.Compose([transforms.ToTensor()])
+    # 下载MNIST （ 当前目录， 指定训练集或测试集，   ）
+    data_set = MNIST("", is_train, transform=to_tensor, download=True)
+    # batchsize 一次15张图片， shuffle为随机
+    return DataLoader(data_set, batch_size=15, shuffle=True)
+
+# 评估神经网络正确率
+def evaluate(test_data, net):
+    n_correct = 0
+    n_total = 0
+    with torch.no_grad():
+        # 取数据
+        for (x, y) in test_data:
+            # 计算预测值
+            outputs = net.forward(x.view(-1, 28*28))
+            # 对数据的结果进行比较，累加正确预测数量
+            for i, output in enumerate(outputs):
+                if torch.argmax(output) == y[i]:
+                    n_correct += 1
+                n_total += 1
+    # 返回正确率
+    return n_correct / n_total
+
+def main():
+
+    train_data = get_data_loader(is_train=True)
+    test_data = get_data_loader(is_train=False)
+    net = Net()
+
+    # 初始网络正确率
+    print("initial accuracy: ", evaluate(train_data, net))
+    # 训练神经网络
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    for epoch in range(2):
+        for (x, y) in train_data:
+            # 初始化
+            net.zero_grad()
+            # 正向传播
+            output = net.forward(x.view(-1, 28*28))
+            # 计算差值, nll_loss对数损失函数 匹配前面对数运算
+            loss = torch.nn.functional.nll_loss(output, y)
+            # 反向误差传播
+            loss.backward()
+            # 优化网络参数
+            optimizer.step()
+        print("epoch: ", epoch,"accuracy: ", evaluate(train_data, net))
+
+    for (n, (x, _)) in enumerate(test_data):
+        # 随机抽三张显示预测结果
+        if n > 3:
+            break
+        predict = torch.argmax(net.forward(x[0].view(-1, 28*28)))
+        plt.figure(n)
+        plt.imshow(x[0].view(28,28))
+        plt.title("prediction: " + str(int(predict)))
+    plt.show()
+
+if __name__ == "__main__":
+    main()
